61488b49f5c7e8fc0a7b65d2ba813d715c237005
ThienVuLe <thienvurock@gmail.com>
2018-01-26 22:21:54 +0700

    change from code to use story board

--- Click Counter/ViewController.swift
+++ Click Counter/ViewController.swift
@@ -11,29 +11,29 @@ import UIKit
 class ViewController: UIViewController {
     
     var count = 0
-    var label:UILabel!
+    @IBOutlet var label:UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //create label show on UI
-        let label = UILabel()
-        label.frame = CGRect.init(x: 150, y: 150, width: 60, height: 60)
-        label.text = "0"
-        self.view.addSubview(label)
-        self.label = label
-        
-        //create button
-        let button = UIButton()
-        button.frame = CGRect.init(x: 150, y: 250, width: 60, height: 60)
-        button.setTitle("Click", for: .normal)
-        button.setTitleColor(UIColor.blue, for: .normal)
-        //hiện lên màn hình
-        self.view.addSubview(button)
-        button.addTarget(self, action: #selector(incrementNumber), for: UIControlEvents.touchUpInside)
+//        //create label show on UI
+//        let label = UILabel()
+//        label.frame = CGRect.init(x: 150, y: 150, width: 60, height: 60)
+//        label.text = "0"
+//        self.view.addSubview(label)
+//        self.label = label
+//
+//        //create button
+//        let button = UIButton()
+//        button.frame = CGRect.init(x: 150, y: 250, width: 60, height: 60)
+//        button.setTitle("Click", for: .normal)
+//        button.setTitleColor(UIColor.blue, for: .normal)
+//        //hiện lên màn hình
+//        self.view.addSubview(button)
+//        button.addTarget(self, action: #selector(incrementNumber), for: UIControlEvents.touchUpInside)
     }
     
-    @objc func incrementNumber() {
+    @IBAction func incrementNumber() {
         self.count = self.count + 1
         self.label.text = "\(self.count)"
     }
